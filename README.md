c6e78f90d6f97cafcb17d45f562c06bbda95c73e
mk-pmb <mk-pmb@users.noreply.github.com>
2015-03-01 14:51:17 +0100

    v0.2.0

---
+++ README.md
@@ -0,0 +1,30 @@
+-*- coding: utf-8, tab-width: 2 -*-
+
+usnam
+=====
+Concise stack traces for node.js:
+[pretty-error](https://github.com/AriaMinaei/pretty-error) **plus**:
+  * compact theme
+    * still with paths
+  * auto-start
+  * some default path aliases
+
+The package name is derrived from "Ausnahme", german for *exception*,
+dialectally contracted to the limits of what I could recognize.
+
+
+Example error display
+---------------------
+![out of memory](https://github.com/mk-pmb/node-usnam-pmb/raw/master/doc/example-error.png)
+
+
+Todo
+----
+  * Extend my terminal's font with 📦 (U+1f4e6 Package) and
+    📂 (U+1f4c2 Open File Folder).
+  * Alias prefixes: use 📦 for packages and 📂 for current directory.
+
+
+License
+-------
+ISC
---
+++ pec.js
@@ -0,0 +1,113 @@
+/* -*- coding: UTF-8, tab-width: 2 -*- */
+/*jslint indent: 2, maxlen: 80, continue: true, unparam: true, node: true */
+'use strict';
+
+var primaryNodeModulesDir, chr = String.fromCharCode,
+  PrettyError = require('pretty-error'),
+  pathLib = require('path'),
+  dirN = pathLib.dirname,
+  pe = new PrettyError();
+
+
+pe.aliasParentDirs = function (path, alias, ups) {
+  var upped = 0, prevPath = '', upAlias = '';
+  ups = (ups === undefined ? 3 : Number(ups));
+  if (ups < 0) { ups = 0; }
+  for (upped; upped <= ups; upped += 1) {
+    upAlias = alias.replace(/\^/g, (upped ? '«' + String(upped) : ''));
+    pe.alias(path + '/', upAlias);
+    prevPath = path;
+    path = pathLib.dirname(path);
+    if (path === prevPath) { return; }
+  }
+};
+
+
+pe.aliasModuleDir = function almd(modName) {
+  var modDir = '';
+  if ('function' === typeof modName.forEach) { return modName.forEach(almd); }
+  try {
+    modDir = dirN(require.resolve(modName));
+  } catch (lookupErr) {
+    return;
+  }
+  pe.alias(modDir + '/', 'mod:' + modName + ':');
+};
+
+
+pe.aliasModuleDir([
+  // common flow-control packages:
+  'async',
+  'bluebird',
+  'lodash',
+  'q',
+]);
+
+
+pe.primaryNodeModulesDir = (function guessPrimaryNodeModulesDir(candidates) {
+  var shortest = dirN(dirN(module.filename));
+  candidates.map(function (candi) {
+    try {
+      candi = dirN(dirN(require.resolve(candi + '/package.json')));
+    } catch (lookupErr) {
+      return;
+    }
+    if (candi.length < shortest.length) { shortest = candi; }
+  });
+  return shortest;
+}([
+  // Use this list of modules to try and find a node modules directory
+  // above the one where you installed p.e.c.:
+  'pretty-error',
+  'lodash',
+  'jslint',
+  'grunt',
+]));
+
+
+pe.aliasParentDirs(pe.primaryNodeModulesDir, 'mod:', 0);
+if ('string' === typeof ((require.main || false).filename)) {
+  pe.aliasParentDirs(dirN(require.main.filename), 'main:', 0);
+}
+pe.aliasParentDirs(process.cwd(), 'cwd:', 0);
+
+pe.appendStyleShortened = function (st) {
+  var rekeyed = {};
+  Object.keys(st).map(function (key) {
+    var val = st[key];
+    key = key.replace(/(^|,\s*)\*/, '$1>trace>item'
+      ).replace(/(^|,\s*)/g, '$1pretty-error').replace(/>/g, ' > ');
+    rekeyed[key] = val;
+  });
+  pe.appendStyle(rekeyed);
+};
+
+
+pe.appendStyleShortened({
+  '': { display: 'block', marginLeft: 0, },
+  '>header>title>kind, >header>colon': {
+    background: 'none',
+    color: 'bright-red',
+  },
+  '>header>title>kind': { margin: 0, padding: 0, },
+  '>trace': { marginTop: 0, marginLeft: 1, },
+  '*': {
+    display: 'block',
+    marginLeft: 2,
+    marginBottom: 0,
+    bullet: '"<grey>→</grey>"'
+  },
+  '*>header': { display: 'inline' },
+  '*>footer': { display: 'inline' },
+  '*>header>pointer>file': { color: 'bright-blue' },
+  '*>header>pointer>colon': { color: 'cyan' },
+  '*>header>pointer>line': { color: 'bright-cyan' },
+  '*>header>what': { color: 'bright-white' },
+  '*>footer>addr': { display: 'inline', color: 'grey', paddingLeft: 1, },
+  '*>footer>extra': { display: 'inline', color: 'yellow', },
+});
+
+
+
+pe.start();
+module.exports = pe;
