7b2c67c0ba01930b85ee10b077226c44926f6362
mk-pmb <mk-pmb@users.noreply.github.com>
2017-02-10 23:58:48 +0100

    v0.2.3: update dependencies

---
+++ test/all.js
@@ -0,0 +1,10 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+require('usnam-pmb');
+
+function a() { throw new RangeError('Oh noez!'); }
+function b() { return a(); }
+function c() { return b(); }
+c();
